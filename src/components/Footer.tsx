e0cdc5e749c175ff56c3d68ce79fc5d559f36970
dawidwawid <dawid.wierzbicki@onet.eu>
2026-02-08 16:40:34 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Row, IconButton, SmartLink, Text } from "@once-ui-system/core";
-import { person, social } from "@/resources";
+import { social } from "@/resources";
 import styles from "./Footer.module.scss";
 
 export const Footer = () => {
@@ -23,13 +23,14 @@ export const Footer = () => {
       >
         <Text variant="body-default-s" onBackground="neutral-strong">
           <Text onBackground="neutral-weak">© {currentYear} /</Text>
-          <Text paddingX="4">{person.name}</Text>
+          <Text paddingX="4">IT’S FRACTAL</Text>
           <Text onBackground="neutral-weak">
             {/* Usage of this template requires attribution. Please don't remove the link to Once UI unless you have a Pro license. */}
             / Build your portfolio with{" "}
             <SmartLink href="https://once-ui.com/products/magic-portfolio">Once UI</SmartLink>
           </Text>
         </Text>
+
         <Row gap="16">
           {social.map(
             (item) =>
@@ -46,6 +47,7 @@ export const Footer = () => {
           )}
         </Row>
       </Row>
+
       <Row height="80" hide s={{ hide: false }} />
     </Row>
   );
