f9e7017d86ec2436390a36e6376b7a0b4617cf20
dawidwawid <dawid.wierzbicki@onet.eu>
2026-06-08 03:09:16 +0200

    Update footer text from 'IT’S FRACTAL' to 'DEWORAL'

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,7 +23,7 @@ export const Footer = () => {
       >
         <Text variant="body-default-s" onBackground="neutral-strong">
           <Text onBackground="neutral-weak">© {currentYear} /</Text>
-          <Text paddingX="4">IT’S FRACTAL</Text>
+          <Text paddingX="4">DEWORAL</Text>
           <Text onBackground="neutral-weak">
             {/* Usage of this template requires attribution. Please don't remove the link to Once UI unless you have a Pro license. */}
             / Build your portfolio with{" "}
