2e7cf8963a9ca46ca22ffdf438a0114f69b33735
dawidwawid <dawid.wierzbicki@onet.eu>
2026-06-08 04:30:41 +0200

    Update once-ui.config.ts

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -14,7 +14,7 @@ import {
 import { home } from "./index";
 
 // IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
-const baseURL: string = "https://itsfractal.com";
+const baseURL: string = "https://dewora.pl";
 
 const routes: RoutesConfig = {
   "/": true,
